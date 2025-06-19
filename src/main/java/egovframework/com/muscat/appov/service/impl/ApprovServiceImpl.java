5a4e801c3911175f9a617a66ead3172fb17aa4ba
kimbab90 <kimbab90@naver.com>
2025-06-19 17:48:40 +0900

    김민식 / 250619 / thymeleaf 테스트

--- src/main/java/egovframework/com/muscat/appov/service/impl/ApprovServiceImpl.java
+++ src/main/java/egovframework/com/muscat/appov/service/impl/ApprovServiceImpl.java
@@ -1,6 +1,8 @@
-package egovframework.com.muscat.appov.service;
+package egovframework.com.muscat.appov.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import egovframework.com.muscat.appov.service.ApprovService;
 
 @Service
 public class ApprovServiceImpl implements ApprovService {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,2 +1,3 @@
-<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
-<jsp:forward page="/approv.do"/>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<jsp:forward page="/main.do" />
