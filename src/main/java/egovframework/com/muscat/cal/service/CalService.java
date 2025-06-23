64b3484c70a96d6f314181c555530a77640578eb
changsu <jeongeunchong83@gmail.com>
2025-06-23 17:36:23 +0900

    정은총 / 25-06-23 / 일정조회

--- src/main/java/egovframework/com/muscat/cal/service/CalService.java
+++ src/main/java/egovframework/com/muscat/cal/service/CalService.java
@@ -1,5 +1,13 @@
 package egovframework.com.muscat.cal.service;
 
+import java.util.List;
+import java.util.Map;
+
+import egovframework.com.muscat.cal.mapper.ScudVO;
+
 public interface CalService {
+	void insertSchedule(ScudVO vo) throws Exception;
+	
+	List<Map> selectScheduleList() throws Exception;
 	
 }
