e3f8f0597013277e857fb8c31dc8fcb96485edde
osegyoung <osk0816@naver.com>
2025-06-30 08:42:59 +0900

    오세경 / 2025-06-30 / 채팅방

---
+++ src/main/java/egovframework/com/muscat/group/mapper/GruopTreeMapper.java
@@ -0,0 +1,10 @@
+package egovframework.com.muscat.group.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+public interface GruopTreeMapper {
+	
+	    List<Map<String, Object>> selectOrgChartData() throws Exception;
+	}
+
--- src/main/java/egovframework/com/muscat/group/service/GroupTreeService.java
+++ src/main/java/egovframework/com/muscat/group/service/GroupTreeService.java
@@ -1,6 +1,11 @@
 package egovframework.com.muscat.group.service;
 
+import java.util.List;
+import java.util.Map;
+
 public interface GroupTreeService {
 		String grouptree();
+
+		List<Map<String, Object>> getTreeData(String id);
 		
 }
