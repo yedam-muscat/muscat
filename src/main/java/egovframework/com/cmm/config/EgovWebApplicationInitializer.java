d91cebfb1d112f2504816a608e3487c8c2c9dcb9
kimbab90 <kimbab90@naver.com>
2025-06-29 11:47:23 +0900

    김민식 / 250629 / security 적용

--- src/main/java/egovframework/com/cmm/config/EgovWebApplicationInitializer.java
+++ src/main/java/egovframework/com/cmm/config/EgovWebApplicationInitializer.java
@@ -108,7 +108,7 @@ public class EgovWebApplicationInitializer implements WebApplicationInitializer 
 			//-------------------------------------------------------------
 			FilterRegistration.Dynamic egovSpringSecurityLoginFilter = servletContext.addFilter("egovSpringSecurityLoginFilter", new EgovSpringSecurityLoginFilter());
 			//로그인 실패시 반활 될 URL설정
-			egovSpringSecurityLoginFilter.setInitParameter("loginURL", "/uat/uia/egovLoginUsr.do");
+			egovSpringSecurityLoginFilter.setInitParameter("loginURL", "/uat/uia/login.do");
 			//로그인 처리 URL설정
 			egovSpringSecurityLoginFilter.setInitParameter("loginProcessURL", "/uat/uia/actionLogin.do");
 			//처리 Url Pattern
